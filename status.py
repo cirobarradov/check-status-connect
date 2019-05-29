bb6000e8d45957eae7fc4830e8efe947f4653311
Ricardo Bravo <rbravo79@gmail.com>
2019-05-29 17:06:55 +0200

    Update status.py

--- status.py
+++ status.py
@@ -3,14 +3,14 @@ import requests
 import time
 import json
 try:
-    r = requests.get('http://{}:{}/connectors'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT']))    
-    connectors=r.json()
-    print(connectors)
-    for connector in connectors:
-    #print(connector)
-        status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
-        status_json=json.dumps(status)
-        print(status_json)
+    while True:
+        r = requests.get('http://{}:{}/connectors'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT']))    
+        connectors=r.json()
+        for connector in connectors:
+            status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
+            status_json=json.dumps(status)
+            print(status_json)
+        sleep(os.environ['SLEEP_TIME'])
 except:
     raise
 
