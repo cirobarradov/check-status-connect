a2f6e363509f0c5f69a8698be2754c541eee6a52
Ricardo Bravo <rbravo79@gmail.com>
2019-05-30 11:49:01 +0200

    Update status.py

--- status.py
+++ status.py
@@ -8,7 +8,7 @@ import json
 try:
     while True:
         r = requests.get('http://{}:{}/connectors'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT']))    
-        request_id=uuid.uuid1()
+        request_id=str(uuid.uuid1())
         connectors=r.json()
         for connector in connectors:
             status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
