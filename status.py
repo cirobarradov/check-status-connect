25ff42a586c4942433f59ffb87975d61d1143f12
Ricardo Bravo <rbravo79@gmail.com>
2019-05-30 14:52:35 +0200

    Update status.py

--- status.py
+++ status.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import uuid
+from datetime import datetime
 
 from time import sleep
 import json
@@ -11,11 +12,13 @@ try:
         request_id=str(uuid.uuid1())
         connectors=r.json()
         for connector in connectors:
+            request_timestamp=str(datetime.now())
             status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
             config=requests.get('http://{}:{}/connectors/{}'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()['config']
             status['config']=config
             status['state']=status.get('connector').get('state')
             status['request_id']=request_id
+            status['request_timestamp']=request_timestamp
             status_json=json.dumps(status)           
             print(status_json)
         sleep(int(os.environ['SLEEP_TIME']))
