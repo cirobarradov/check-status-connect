a37e29f118ad33d823f944c17cd9fdb77faba639
Ricardo Bravo <rbravo79@gmail.com>
2019-05-30 07:54:52 +0200

    Update status.py

--- status.py
+++ status.py
@@ -8,9 +8,10 @@ try:
         connectors=r.json()
         for connector in connectors:
             status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
+            config=requests.get('http://{}:{}/connectors/{}'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
+            status['config']=config
             status_json=json.dumps(status)
             print(status_json)
         sleep(int(os.environ['SLEEP_TIME']))
 except:
     raise
-
