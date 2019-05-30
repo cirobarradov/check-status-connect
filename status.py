9b6994e764b155a2d1b9b8e3a6fedb45088ea5c8
Ricardo Bravo <rbravo79@gmail.com>
2019-05-30 10:18:34 +0200

    Update status.py

--- status.py
+++ status.py
@@ -3,15 +3,8 @@ import requests
 
 from time import sleep
 import json
-import logging
-from pythonjsonlogger import jsonlogger
-try:
-    logger = logging.getLogger()
-    logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
-    logger.addHandler(logHandler)
 
+try:
     while True:
         r = requests.get('http://{}:{}/connectors'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT']))    
         connectors=r.json()
@@ -19,8 +12,9 @@ try:
             status=requests.get('http://{}:{}/connectors/{}/status'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()
             config=requests.get('http://{}:{}/connectors/{}'.format(os.environ['CONNECT_HOST'],os.environ['CONNECT_PORT'],connector)).json()['config']
             status['config']=config
+            status['state']=status.get('connector').get('state')
             status_json=json.dumps(status)           
-            logger.info(status_json)
+            print(status_json)
         sleep(int(os.environ['SLEEP_TIME']))
 except:
     raise
